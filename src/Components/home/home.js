dcf1439c5111b4225a60490406a3b32f50b34dfb
Ty Crockett <ty.crockett@getweave.com>
2019-01-25 17:15:32 -0700

    Everything baby!

--- src/Components/home/home.js
+++ src/Components/home/home.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import main from '../../Assets/main.jpg';
 import amazon from '../../Assets/amazon.png';
 import venmo from '../../Assets/venmo.png';
+import linds from '../../Assets/linds.png';
+import ty from '../../Assets/ty.png';
+
 import './home.css';
 
 export default class Navbar extends React.Component {
@@ -48,6 +51,37 @@ export default class Navbar extends React.Component {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div>
+                    <p className="aboutus" id="aboutus">About Us</p>
+                    <div className="aboutusp">
+                        <div className="convoher">
+                            <div className="imgDiv"><img src={linds} alt='linds' className='herIMG'/></div>
+                            <p className="her together">My friend from institute, Justin, reached out to me and asked if I would be interested in going out with a friend of his. I told him I’d be open to it, but had a lot going on at the time. I meant to get back to him but I didn’t. Luckily, Ty didn’t give up easily so he reached out to me on Facebook until I responded. We went on a date soon after and connected instantly. I didn’t know that I had gone out with my future husband, but I knew I wanted to see him again.</p>
+                        </div>
+
+                        <div className="convohim">
+                            <p className="him together">One of my best friends, Justin, shared a class with Lindsay and they ended up being friends on facebook. I saw they were friends and thought, “she’s dang cute” so I requested her as a friend and sent her a "wave". She didn’t get back to me so I asked Justin if he could set me up, which he said yes and tried. She ended up being too busy, but she was too dang cute to let go. A month later I messaged her again on facebook and asked her out. She finally said yes!</p>
+                            <div className="imgDiv"><img src={ty} alt='ty' className='himIMG'/></div>
+                        </div>
+
+                        <p className="us together">We had so much fun together and never stopped laughing. We have spent almost every day together since. We knew we wanted to get marrired within a month of meeting for the first time.</p>
+                        <br/>
+                        <hr/>
+                        <p className="them">The Proposal</p>
+                        <hr/>
+
+                        <div className="convoher">
+                            <div className="imgDiv"><img src={linds} alt='linds' className='herIMG'/></div>
+                            <p className="her together">Ty took me to the restaurant we went to on our first date. Afterward he surprised me by taking me to a spot near Utah Lake where he had set up a lit arch and got down on one knee. Our special moment was almost ruined when the arch fell over and nearly knocked us out.</p>
+                        </div>
+
+                        <div className="convohim">
+                            <p className="him together">Lindsay wouldn’t stop singing “And a knick knack paddy whack, give the dog a bone!” in the car on the way to Utah lake. Her singing is still on of the reasons I love her. That was a very special night and when she said yes to me I was elated.</p> 
+                            <div className="imgDiv"><img src={ty} alt='ty' className='himIMG'/></div>
+                        </div>
+                        <p className="them">We’re very excited to get married in the Provo City Center Temple, and for our friends and family to be joining us at our wedding celebration at the Vista at Cedar Hills!</p>
                     </div>
                 </div>
             </div>
--- src/Components/navbar/navbar.js
+++ src/Components/navbar/navbar.js
@@ -11,7 +11,7 @@ export default class Navbar extends React.Component {
                 </div>
                 <div className="rightNav">
                     <ul className="flexStyle">
-                        <li className="list">About Us</li>
+                        <a className="list" href="#aboutus">About Us</a>
                         <a href="https://docs.google.com/forms/d/e/1FAIpQLScyPUBv0OOaM8yg3lRRkhgzybl3i7Pb_VHn7VXjpf-d6J7hbw/viewform" className="list"><li >RSVP</li></a>
                         <a href="https://goo.gl/maps/AowMFr7QTsP2"><li className="list">Directions</li></a>
                     </ul>
